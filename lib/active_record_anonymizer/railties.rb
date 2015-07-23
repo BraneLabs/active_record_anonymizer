54f745d9b3f63e399a8ca1acbb0db53e5fbad1d7
Daniel Naves de Carvalho <danielnc@gmail.com>
2015-07-23 10:25:53 -0700

    removing unecessary load of databases.rake

--- lib/active_record_anonymizer/railties.rb
+++ lib/active_record_anonymizer/railties.rb
@@ -1,7 +1,6 @@
 module ActiveRecordAnonymizer
   class Railtie < Rails::Railtie
     rake_tasks do
-      import "active_record/railties/databases.rake"
       import "tasks/views.rake"
     end
   end
