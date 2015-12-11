b7a190b98750bd8af4dc4a6ec17282353d1b6929
Daniel Naves de Carvalho <danielnc@gmail.com>
2015-12-11 09:54:42 -0200

    Checking to see if connection adaptor responds to schema_search_path

--- lib/active_record_anonymizer/postgresql/pg_tools.rb
+++ lib/active_record_anonymizer/postgresql/pg_tools.rb
@@ -12,12 +12,14 @@ module ActiveRecordAnonymizer
       end
 
       def set_anonymized_search_path(name=ActiveRecordAnonymizer::Anonymizer.default_schema_name)
-        path_parts = [name.to_s, "public"]
-        ActiveRecord::Base.connection.schema_search_path = path_parts.join(",")
+        if ActiveRecord::Base.connection.respond_to?(:schema_search_path=)
+          path_parts = [name.to_s, "public"]
+          ActiveRecord::Base.connection.schema_search_path = path_parts.join(",")
+        end
       end
 
       def restore_default_search_path
-        ActiveRecord::Base.connection.schema_search_path = default_search_path
+        ActiveRecord::Base.connection.schema_search_path = default_search_path if ActiveRecord::Base.connection.respond_to?(:schema_search_path=)
       end
     end
   end
