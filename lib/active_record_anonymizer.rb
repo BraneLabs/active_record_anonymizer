02947ba5e984df83eebd580895cf65331d8a2325
Daniel Naves de Carvalho <danielnc@gmail.com>
2015-12-11 11:15:08 -0200

    adding dependencies using require

--- lib/active_record_anonymizer.rb
+++ lib/active_record_anonymizer.rb
@@ -5,10 +5,12 @@ require 'active_support'
 require "active_record_anonymizer/version"
 require "active_record_anonymizer/postgresql/pg_tools.rb"
 require "active_record_anonymizer/anonymizer.rb"
+require "active_record_anonymizer/strategies/base_anonymizer.rb"
 require "active_record_anonymizer/strategies/text_anonymizer.rb"
 require "active_record_anonymizer/strategies/number_anonymizer.rb"
 require "active_record_anonymizer/strategies/email_anonymizer.rb"
 require "active_record_anonymizer/strategies/birth_date_anonymizer.rb"
+require "active_record_anonymizer/strategies/empty_anonymizer.rb"
 
 require "rspec/anonymizer/matchers.rb"
 
