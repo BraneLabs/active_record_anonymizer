4dff8ab86b416c02fc73aa23da7a4e42fece4e80
Daniel Naves de Carvalho <danielnc@gmail.com>
2016-06-14 18:07:29 -0300

    Allow usage of SET LOCAL transaction values.

--- lib/active_record_anonymizer/postgresql/pg_tools.rb
+++ lib/active_record_anonymizer/postgresql/pg_tools.rb
@@ -11,15 +11,24 @@ module ActiveRecordAnonymizer
         @default_search_path ||= %{"$user", public}
       end
 
-      def set_anonymized_search_path(name=ActiveRecordAnonymizer::Anonymizer.default_schema_name)
-        if ActiveRecord::Base.connection.respond_to?(:schema_search_path=)
-          path_parts = [name.to_s, "public"]
-          ActiveRecord::Base.connection.schema_search_path = path_parts.join(",")
+      def set_anonymized_search_path(options = {})
+        name = options[:connection_name] || ActiveRecordAnonymizer::Anonymizer.default_schema_name
+
+        path_parts = [name.to_s, "public"].join(",")
+
+        if options[:local_transaction_values]
+          ActiveRecord::Base.connection.execute("SET LOCAL search_path TO #{path_parts}", 'SCHEMA')
+        elsif ActiveRecord::Base.connection.respond_to?(:schema_search_path=)
+          ActiveRecord::Base.connection.schema_search_path = path_parts
         end
       end
 
-      def restore_default_search_path
-        ActiveRecord::Base.connection.schema_search_path = default_search_path if ActiveRecord::Base.connection.respond_to?(:schema_search_path=)
+      def restore_default_search_path(options = {})
+        if options[:local_transaction_values]
+          ActiveRecord::Base.connection.execute("SET LOCAL search_path TO #{default_search_path}", 'SCHEMA')
+        elsif ActiveRecord::Base.connection.respond_to?(:schema_search_path=)
+          ActiveRecord::Base.connection.schema_search_path = default_search_path
+        end
       end
     end
   end
