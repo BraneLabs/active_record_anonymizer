e540eb0aabc171fabd98f07c4a50081e4d8918aa
Daniel Naves de Carvalho <danielnc@gmail.com>
2016-06-15 10:35:39 -0300

    Making explicit the option to use transactional search_path for pgtools

--- lib/active_record_anonymizer/postgresql/pg_tools.rb
+++ lib/active_record_anonymizer/postgresql/pg_tools.rb
@@ -13,21 +13,47 @@ module ActiveRecordAnonymizer
 
       def set_anonymized_search_path(options = {})
         name = options[:connection_name] || ActiveRecordAnonymizer::Anonymizer.default_schema_name
-
         path_parts = [name.to_s, "public"].join(",")
 
-        if options[:local_transaction_values]
-          ActiveRecord::Base.connection.execute("SET LOCAL search_path TO #{path_parts}", 'SCHEMA')
-        elsif ActiveRecord::Base.connection.respond_to?(:schema_search_path=)
+        if ActiveRecord::Base.connection.respond_to?(:schema_search_path=)
           ActiveRecord::Base.connection.schema_search_path = path_parts
         end
       end
 
       def restore_default_search_path(options = {})
-        if options[:local_transaction_values]
-          ActiveRecord::Base.connection.execute("SET LOCAL search_path TO #{default_search_path}", 'SCHEMA')
-        elsif ActiveRecord::Base.connection.respond_to?(:schema_search_path=)
+        if ActiveRecord::Base.connection.respond_to?(:schema_search_path=)
           ActiveRecord::Base.connection.schema_search_path = default_search_path
+        end
+      end
+
+      def set_transactional_anonymized_search_path(options = {})
+        return unless block_given?
+
+        name = options[:connection_name] || ActiveRecordAnonymizer::Anonymizer.default_schema_name
+        path_parts = [name.to_s, "public"].join(",")
+
+        ActiveRecord::Base.transaction do
+          ActiveRecord::Base.connection.execute("SET LOCAL search_path TO #{path_parts}", 'SCHEMA')
+          begin
+            yield
+          ensure
+            ActiveRecord::Base.connection.execute("SET LOCAL search_path TO #{default_search_path}", 'SCHEMA')
+          end
+        end
+      end
+
+      def set_transactional_default_search_path(options = {})
+        return unless block_given?
+
+        old_path = search_path
+
+        ActiveRecord::Base.transaction do
+          ActiveRecord::Base.connection.execute("SET LOCAL search_path TO #{default_search_path}", 'SCHEMA')
+          begin
+            yield
+          ensure
+            ActiveRecord::Base.connection.execute("SET LOCAL search_path TO #{old_path}", 'SCHEMA')
+          end
         end
       end
     end
